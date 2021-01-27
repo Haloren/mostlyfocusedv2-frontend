c5e86ea0b5985c4c27cac41aee6c8380169e2cb8
Haloren <mckennakyler@gmail.com>
2021-01-27 00:55:44 -0700

    added some titles to Help component

--- src/components/Help.js
+++ src/components/Help.js
@@ -10,7 +10,15 @@ const Help = () => {
                 <br></br>
                 <hr></hr>
                 <br></br>
-                <h3>Title goes here</h3>
+                <h3>How to use</h3>
+                <ul>
+                    <li>Help keep yourself focused by setting this as your home page, that way everytime you open a new tab to search the internet you'll be reminded of current Todos and upcoming Events.</li>
+                    <li>Some text</li>
+                </ul>
+                <br></br>
+                <hr></hr>
+                <br></br>
+                <h3>Todos</h3>
                 <ul>
                     <li>Some text</li>
                     <li>Some text</li>
@@ -18,7 +26,7 @@ const Help = () => {
                 <br></br>
                 <hr></hr>
                 <br></br>
-                <h3>Title goes here</h3>
+                <h3>Events</h3>
                 <ul>
                     <li>Some text</li>
                     <li>Some text</li>
@@ -26,7 +34,7 @@ const Help = () => {
                 <br></br>
                 <hr></hr>
                 <br></br>
-                <h3>Title goes here</h3>
+                <h3>Weather</h3>
                 <ul>
                     <li>Some text</li>
                     <li>Some text</li>
