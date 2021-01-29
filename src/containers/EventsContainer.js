7ae46023aba6cf6f3b7a02e3b2db6852a1e2fd35
Haloren <mckennakyler@gmail.com>
2021-01-29 01:19:50 -0700

    added event and todo forms to their containers

--- src/containers/EventsContainer.js
+++ src/containers/EventsContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Events from '../components/Events'
+import Events from '../components/Events';
+import EventForm from '../components/EventForm';
 
 class EventsContainer extends React.Component {
     render() {
         return (
             <>
+                <EventForm />
                 <Events />
             </>
         )
--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Todos from '../components/Todos'
+import TodoForm from '../components/TodoForm';
 
 class TodosContainer extends React.Component {
     render() {
         return (
             <>
+                <TodoForm />
                 <Todos />
             </>
         )
