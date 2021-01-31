bbb2d03d8842995e195e1dde5e25e4b7c773ad98
Haloren <mckennakyler@gmail.com>
2021-01-31 01:50:33 -0700

    renders event list items

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 const Events = (props) => {
-    // console.log("Events", props.user)
+    // console.log("Events", props.user.events)
 
     return (
-        <>
+        <div className="events-container">
            <h1> Upcoming Events </h1>
-        </>
+           <ul>
+               {props.user.events && props.user.events.map(event =>
+                    <li key={event.id} className="list-item">
+                        <h3> {event.date} </h3>
+                        <h3> {event.title} </h3>
+                        <button className="delete-btn">X</button>
+                    </li>
+                )}
+           </ul>
+        </div>
     )
 }
 
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
 const Todos = (props) => {
-    console.log("Todos", props.user.todos)
-
+    // console.log("Todos", props.user.todos)
     return (
-        <>
+        <div className="todos-container">
             <h1> Todos List </h1>  
             <ul>
                 {props.user.todos && props.user.todos.map(todo =>
-                    <li key={todo.id} className="todo-item">
+                    <li key={todo.id} className="list-item">
                         <input type="checkbox" className="checkbox"></input>
                         {todo.item}
-                        <button className="delete-btn">X</button>
+                        <button className="delete-btn"> X </button>
                     </li>
                 )}
             </ul>
-        </>
+        </div>
     )
 }
 
