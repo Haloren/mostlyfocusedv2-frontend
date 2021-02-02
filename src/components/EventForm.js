13d5f5332905051308fc78e8f8c2c013a9cb4acf
Haloren <mckennakyler@gmail.com>
2021-02-01 23:19:04 -0700

    started todo & event form layout

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 class EventForm extends React.Component {
 
-    state = { }
+    state = { title: '', date: '' }
 
     render() {
         return (
             <div>
-                
+                <form onSubmit={}>
+                    <input type="text" name="title" value={} onChange={} placeholder="Add an Event" required ></input>
+                    <input type="date" name="date" value={} onChange={} required ></input>
+                    <input type="submit" value="+"></input>
+                </form>
             </div>
         )
     }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -7,7 +7,11 @@ class TodoForm extends React.Component {
     render() {
         return (
             <div>
-                
+                <form onSubmit={}>
+                    <input type="text" name="title" value={} onChange={} placeholder="Add an Todo" required ></input>
+                    <input type="date" name="date" value={} onChange={} required ></input>
+                    <input type="submit" value="+"></input>
+                </form>
             </div>
         )
     }
