d438ed52cf1c108c71a91c5aafe23b0056b66c38
Haloren <mckennakyler@gmail.com>
2021-02-03 20:26:51 -0700

    removed forms container, will revisit

--- src/components/UsersData.js
+++ src/components/UsersData.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Todos from '../components/Todos';
 import Events from '../components/Events';
-import FormsContainer from '../containers/FormsContainer';
 
 const UsersData = (props) => {
     // console.log("UsersData", props.users[0])
@@ -14,7 +13,6 @@ const UsersData = (props) => {
             {/* <h1>Hello {props.users[0].name}</h1> */}
             <Todos user = { user } />
             <Events user = { user } />
-            <FormsContainer user = { user } />
         </>
     )
 }
